07f89bf85b1442d5a0a5ab2f5dbd495bd7e24afa
longtian <longtian2012@foxmail.com>
2023-11-16 20:02:35 +0800

    装饰器改造

--- api/index.py
+++ api/index.py
@@ -1,9 +1,21 @@
 from flask import Blueprint,jsonify
-from decorators.decorators import token_required
+from decorators.decorators import token_role_required
+from models.models import User
 
 index = Blueprint('index',__name__)
 
 @index.route('/')
-@token_required
+@token_role_required()
 def show(current_user):
-    return jsonify('pong!')
+    users = User.query.all()
+
+    user_list = []
+    for user in users:
+        user_data = {
+            'id': user.id,
+            'name': user.name,
+            'email': user.email
+        }
+        user_list.append(user_data)
+
+    return jsonify({'users': user_list})
--- decorators/decorators.py
+++ decorators/decorators.py
@@ -4,26 +4,28 @@ from flask import json, jsonify, request, current_app
 from models.models import User
 
 # JWT校验
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-        # 获取token
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        # token校验不通过，返回401
-        if not token:
-            return jsonify({'message' : 'Token is missing !!'}), 401
-        try:
-            # 解码负载以获取存储的详细信息
-            data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
-            current_user = User.query\
-                .filter_by(public_id = data['public_id'])\
-                .first()
-            if current_user.role != 'admin':
-                return jsonify({'message' : 'Permission denied !!'}), 403
-        except:
-            return jsonify({'message' : 'Token is invalid !!'}), 401
-        # 将当前登录用户的上下文返回给路由
-        return  f(current_user, *args, **kwargs)
-    return decorated
+def token_role_required(role = None):
+    def token_required(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            token = None
+            # 获取token
+            if 'x-access-token' in request.headers:
+                token = request.headers['x-access-token']
+            # token校验不通过，返回401
+            if not token:
+                return jsonify({'message' : 'Token is missing !!'}), 401
+            try:
+                # 解码负载以获取存储的详细信息
+                data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
+                current_user = User.query\
+                    .filter_by(public_id = data['public_id'])\
+                    .first()
+                if role is not None and current_user.role != role:
+                    return jsonify({'message' : 'Permission denied !!'}), 403
+            except:
+                return jsonify({'message' : 'Token is invalid !!'}), 401
+            # 将当前登录用户的上下文返回给路由
+            return f(current_user, *args, **kwargs)
+        return decorated
+    return token_required
