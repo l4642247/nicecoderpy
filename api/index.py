a8d2ecf759f5953f497fc0f2643b25448966262f
l4642247 <240467383@qq.com>
2024-04-16 12:55:19 +0800

    session

--- api/index.py
+++ api/index.py
@@ -1,7 +1,18 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,current_app,redirect,session
 from models.redis_client import RedisClient
 index = Blueprint('index',__name__)
 
 @index.route('/')
 def home():
     return render_template('home.html')
+
+@index.route('/test')
+def test():
+    # 用于前端取值
+    session["user_info"] = {'user_id': 123, 'name': 'name'}
+
+    # 打印session的值
+    current_app.logger.info(f"Session: {session}")
+    return redirect('/')
+
+
