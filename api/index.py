34514bacf9e7fd7682093bda1fdde78b7354eaa2
longtian <240467383@qq.com>
2024-04-23 23:13:36 +0800

    +1

--- api/index.py
+++ api/index.py
@@ -1,7 +1,8 @@
 from flask import Blueprint,request,render_template,current_app,redirect,session,jsonify
 from models.redis_client import RedisClient
 from decorators.decorators import token_role_required
-from models.models import User, db
+from models.models import User, MessageLog, db
+from sqlalchemy import desc
 index = Blueprint('index',__name__)
 
 @index.route('/')
@@ -13,10 +14,20 @@ def home():
 def admin():
     return render_template('admin/index.html')
 
+@index.route('/admin/console')
+# @token_role_required(role='admin')
+def console():
+    return render_template('admin/console.html')
+
 @index.route('/admin/user')
 # @token_role_required(role='admin')
 def user():
     return render_template('admin/user.html')
+
+@index.route('/admin/message')
+# @token_role_required(role='admin')
+def message():
+    return render_template('admin/message.html')
 
 @index.route('/admin/user/page')
 # @token_role_required(role='admin')
@@ -25,7 +36,16 @@ def userPage():
     per_page = request.args.get('limit', 10, type=int)
     users = User.query.paginate(page=page, per_page=per_page, error_out=False)
     serialized_users = [user.serialize() for user in users.items]
-    response_data = {"code": 0, "data": serialized_users}
+    response_data = {"code": 0, "data": serialized_users, "count": users.total}
     return jsonify(response_data)
 
+@index.route('/admin/message/page')
+# @token_role_required(role='admin')
+def messagePage():
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('limit', 10, type=int)
+    messages = MessageLog.query.order_by(desc(MessageLog.create_time)).paginate(page=page, per_page=per_page, error_out=False)
+    serialized_messages = [message.serialize() for message in messages.items]
+    response_data = {"code": 0, "data": serialized_messages, "count": messages.total}
+    return jsonify(response_data)
 
